714fe5cef65b78555c8235b20e9001a67e1fbd2b
HsinYeh <thankyouyou06@gmail.com>
2019-09-09 14:01:05 +0800

    modified:   xtalk_plot.C

--- xtalk_plot.C
+++ xtalk_plot.C
@@ -6,25 +6,46 @@ void xtalk_plot(){
     f_datainput >> filename;
     f.open(filename);
 
-    double xtalk[128];
+    double xtalk_avg[128];
     double channelID[128];
     while(!f.eof()){
 	int chip, ch;
-	double x;
-	f >> chip >> ch >> x;
-	xtalk[chip*32 + ch/2] = x;
+	double avg, intersect, slope;
+	f >> chip >> ch >> avg >> intersect >> slope;
+	xtalk_avg[chip*32 + ch/2] = avg;
+	xtalk_intersect[chip*32 + ch/2]  = intersect;
+	xtalk_slope[chip*32 + ch/2]  = slope;
     }
     for(int ich = 0; ich < 128; ich++){
 	channelID[ich] = ich*2;
     }
 
     TCanvas *c = new TCanvas();
-    TGraph *g = new TGraph(128, channelID, xtalk);
-    g->SetMarkerStyle(22);
-    g->GetXaxis()->SetTitle("channelID");
-    g->GetYaxis()->SetTitle(" < EFirstRing / EInj > ");
-    g->SetTitle("charge injection xtalk");
-    g->Draw("AP");
+    TGraph *g_avg = new TGraph(128, channelID, xtalk_avg);
+    g_avg->SetMarkerStyle(22);
+    g_avg->GetXaxis()->SetTitle("channelID");
+    g_avg->GetYaxis()->SetTitle(" < EFirstRing / EInj > ");
+    g_avg->SetTitle("charge injection xtalk");
+    g_avg->Draw("AP");
     c->Update();
     c->SaveAs("output.pdf");
+
+    TGraph *g_intersect = new TGraph(128, channelID, xtalk_intersect);
+    g_intersect->SetMarkerStyle(22);
+    g_intersect->GetXaxis()->SetTitle("channelID");
+    g_intersect->GetYaxis()->SetTitle(" < EFirstRing / EInj > ");
+    g_intersect->SetTitle("charge injection xtalk");
+    g_intersect->Draw("AP");
+    c->Update();
+    c->SaveAs("output.pdf");
+
+    TGraph *g_slope = new TGraph(128, channelID, xtalk_slope);
+    g_slope->SetMarkerStyle(22);
+    g_slope->GetXaxis()->SetTitle("channelID");
+    g_slope->GetYaxis()->SetTitle(" slope ");
+    g_slope->SetTitle("charge injection xtalk");
+    g_slope->Draw("AP");
+    c->Update();
+    c->SaveAs("output.pdf");
+
 }
