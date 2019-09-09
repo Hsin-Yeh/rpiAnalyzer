915fe8f3e37aeac341cdc5810b9c826d5cab12cf
HsinYeh <thankyouyou06@gmail.com>
2019-09-09 11:56:18 +0800

    new file:   xtalk_plot.C

---
+++ xtalk_plot.C
@@ -0,0 +1,26 @@
+void xtalk_plot(){
+    ifstream f;
+    ifstream f_datainput;
+    char filename[100];
+    f_datainput.open("data_input.txt");
+    f_datainput >> filename;
+    f.open(filename);
+
+    double xtalk[128];
+    int channelID[128];
+    while(!f.eof()){
+	int chip, ch;
+	double x;
+	f >> chip >> ch >> x;
+	xtalk[chip*32 + ch/2] = x;
+    }
+    for(int ich = 0; ich < 128; ich++){
+	channelID[ich] = ich*2;
+    }
+
+    TCanvas *C = new TCanvas();
+    TGraph *g = new TGraph(128, channelID, xtalk);
+    g->Draw("AP");
+    c->Update();
+    c->SaveAs("output.pdf");
+}
