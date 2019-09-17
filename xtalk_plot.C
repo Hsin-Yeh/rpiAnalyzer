f1308eaf99d6cc327ec9d94e6bd2d775f3ca18b2
HsinYeh <thankyouyou06@gmail.com>
2019-09-17 10:57:04 +0800

    modified:   src/makePlots.cc 	modified:   xtalk_plot.C

--- xtalk_plot.C
+++ xtalk_plot.C
@@ -16,6 +16,10 @@ void readmap(){
     //Since there is no such pad, assign a unreasonable value
     CHmap[2*32+60/2] = make_pair(1000.,1000.);
 
+    for(int ich = 0; ich < 128; ich++){
+	cout << CHmap[ich].first << CHmap[ich].second << endl;
+    }
+
 }
 
 
@@ -44,6 +48,9 @@ void InitTH2Poly(TH2Poly& poly)
 }
 
 void xtalk_plot(){
+
+    readmap();
+    
     ifstream f;
     ifstream f_datainput;
     char filename[100];
@@ -55,6 +62,14 @@ void xtalk_plot(){
     double xtalk_intersect[128];
     double xtalk_slope[128];
     double channelID[128];
+
+    for(int ich = 0; ich < 128; ich++){
+	channelID[ich] = ich*2;
+	xtalk_intersect[ich] = 0;
+	xtalk_avg[ich] = 0;
+	xtalk_slope[ich] = 0;
+    }
+
     while(!f.eof()){
 	int chip, ch, icell;
 	double avg, posx, posy, intersect, slope;
@@ -63,9 +78,6 @@ void xtalk_plot(){
 	xtalk_avg[chip*32 + ch/2] = avg;
 	xtalk_intersect[chip*32 + ch/2]  = intersect;
 	xtalk_slope[chip*32 + ch/2]  = slope;
-    }
-    for(int ich = 0; ich < 128; ich++){
-	channelID[ich] = ich*2;
     }
 
     TCanvas *c = new TCanvas();
@@ -76,8 +88,8 @@ void xtalk_plot(){
     g_avg->SetTitle("charge injection xtalk");
     g_avg->Draw("AP");
     c->Update();
-    gPad->WaitPrimitive();
-    c->SaveAs("output.pdf");
+    //gPad->WaitPrimitive();
+    c->SaveAs("avg.pdf");
 
     TH2Poly *poly = new TH2Poly;
     InitTH2Poly(*poly);
@@ -89,12 +101,14 @@ void xtalk_plot(){
 	bool NoisyBool = false;
 	X = CHmap[forCH].first;
 	Y = CHmap[forCH].second;
+	cout << X << " " << Y << " " << xtalk_intersect[ichannel/2] << endl;
 	poly->Fill(X,Y,xtalk_intersect[ichannel/2]);
     }
     poly->Draw("colztext");
     c->Update();
-    gPad->WaitPrimitive();
-    
+    c->SaveAs("poly.pdf");
+    //gPad->WaitPrimitive();
+
     TGraph *g_intersect = new TGraph(128, channelID, xtalk_intersect);
     g_intersect->SetMarkerStyle(22);
     g_intersect->GetXaxis()->SetTitle("channelID");
@@ -102,8 +116,8 @@ void xtalk_plot(){
     g_intersect->SetTitle("charge injection xtalk");
     g_intersect->Draw("AP");
     c->Update();
-    gPad->WaitPrimitive();
-    c->SaveAs("output.pdf");
+    //gPad->WaitPrimitive();
+    c->SaveAs("intersect.pdf");
 
     TGraph *g_slope = new TGraph(128, channelID, xtalk_slope);
     g_slope->SetMarkerStyle(22);
@@ -112,8 +126,8 @@ void xtalk_plot(){
     g_slope->SetTitle("charge injection xtalk");
     g_slope->Draw("AP");
     c->Update();
-    gPad->WaitPrimitive();
-    c->SaveAs("output.pdf");
+    //gPad->WaitPrimitive();
+    c->SaveAs("slope.pdf");
 
 }
 
